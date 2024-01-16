43ab81fc70f44ccab98fa38887b6db845854b110
bsshark <abrahamlega@hotmail.com>
2024-01-16 09:28:43 +0000

    Installed dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      Inicio Proyecto
-    </>
+    <h1 className="text-3xl font-bold underline bg-gradient-to-b from-green-400 to-blue-400">
+      Hello world!
+    </h1>
   )
 }
 

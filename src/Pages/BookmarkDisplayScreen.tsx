f1249f1263e89afd3bc366addd7bbfec5db1cd43
Abraham <abrahamlega@gmail.com>
2024-01-19 17:54:13 +0000

    small fixes

--- src/Pages/BookmarkDisplayScreen.tsx
+++ src/Pages/BookmarkDisplayScreen.tsx
@@ -1,29 +1,23 @@
-import { useEffect } from "react";
 import { useMovieStore } from "../hooks/useMovieStore";
-import { useAppDispatch } from "../hooks/dispatch";
 import { LoadingComponent } from "../components/LoadingComponent";
 import { MovieCard } from "../components/MovieCard";
 
 export const BookmarkDisplayScreen = () => {
-
 	const { isLoading, bookmarkMovies } = useMovieStore();
 
-	
-	
-	if(isLoading) return <LoadingComponent/>
+	if (isLoading) return <LoadingComponent />;
 
 	return (
 		<div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 h-full grid-rows-2 gap-4 px-5">
 			{bookmarkMovies.length > 0 ? (
-					[...bookmarkMovies]
-						.map((movie) => (
-							<div key={movie.id}>
-								<MovieCard movie={movie} />
-							</div>
-						))
-				) : (
-					<></>
-				)}
-        </div>
+				[...bookmarkMovies].map((movie) => (
+					<div key={movie.id}>
+						<MovieCard movie={movie} />
+					</div>
+				))
+			) : (
+				<></>
+			)}
+		</div>
 	);
 };
